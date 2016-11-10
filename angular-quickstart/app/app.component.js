31798b3e23d71f658d10972f4cebad0db310116d
Tom <yangg@shinetechchina.com>
2016-11-10 18:40:47 +0800

    add list for heros

--- angular-quickstart/app/app.component.js
+++ angular-quickstart/app/app.component.js
@@ -11,15 +11,39 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.heroes = HEROES;
+        this.hero = {
+            id: 1,
+            name: 'windstorm'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'My-app',
-            template: '<h1>my first angular app1111</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details</h2>\n\n    <h2>my heroes</h2>\n    <ul class=\"heroes\">\n        <li *ngFor=\"let hero of heroes\">\n            <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n    </ul>\n\n    <div><label>Id:</label>{{hero.id}}</div>\n    <div>\n        <label>Name:</label>\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n    </div>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -1,8 +1,50 @@
 import {Component} from '@angular/core';
 
+
+
 @Component({
     selector:'My-app',
-    template:'<h1>my first angular app1111</h1>'
+    template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+
+    <h2>my heroes</h2>
+    <ul class="heroes">
+        <li *ngFor="let hero of heroes">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+    </ul>
+
+    <div><label>Id:</label>{{hero.id}}</div>
+    <div>
+        <label>Name:</label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+    </div>`
 })
 
-export class AppComponent{}
+export class AppComponent{
+    title = 'Tour of Heroes';
+    heroes = HEROES;
+    hero : Hero={
+        id:1,
+        name:'windstorm'
+    };
+}
+
+export class Hero{
+    id:number;
+    name:string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
