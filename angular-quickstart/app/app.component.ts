51f7383284a194b94b279270bc9fc69e9d4e2dbb
Tom <yangg@shinetechchina.com>
2016-11-18 17:42:46 +0800

    list heros and add click method and css

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -6,20 +6,81 @@ import {Component} from '@angular/core';
     selector:'My-app',
     template:`
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details</h2>
+
+     <h2>{{hero.name}} details</h2>    
+
+        <div><label>Id:</label>{{hero.id}}</div>
+        <div>
+            <label>Name:</label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+    
+    <div *ngIf="selectedHero">    
+        <h2>{{selectedHero.name}} details</h2>    
+
+        <div><label>Id:</label>{{selectedHero.id}}</div>
+        <div>
+            <label>Name:</label>
+            <input [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
+    </div>
 
     <h2>my heroes</h2>
     <ul class="heroes">
-        <li *ngFor="let hero of heroes">
+        <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero==selectedHero">
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
-    </ul>
+    </ul>`,
 
-    <div><label>Id:</label>{{hero.id}}</div>
-    <div>
-        <label>Name:</label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+        `]
 })
 
 export class AppComponent{
@@ -29,6 +90,11 @@ export class AppComponent{
         id:1,
         name:'windstorm'
     };
+
+    selectedHero:Hero;
+    onSelect(hero:Hero):void{
+        this.selectedHero=hero;
+    }
 }
 
 export class Hero{
@@ -48,3 +114,5 @@ const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
+
