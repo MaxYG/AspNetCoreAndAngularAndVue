d97bc3c590bb378efcde33224a03bd7505984e17
TomYangGang <yangg@shinetechchina.com>
2019-01-06 15:16:25 +0800

    change web

---
+++ AngularjsQuickStart/AngularQS.Web/src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+
+// import { HomeComponent } from './home';
+// import { AuthGuard } from './_guards';
+
+const appRoutes: Routes = [
+    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
