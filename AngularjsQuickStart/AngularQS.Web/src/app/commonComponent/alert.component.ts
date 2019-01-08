4b933d7ee6f8fae64c8e07c4a98abf12cdb1cf22
TomYangGang <yangg@shinetechchina.com>
2019-01-08 22:50:57 +0800

    fixed alert array cache

--- AngularjsQuickStart/AngularQS.Web/src/app/commonComponent/alert.component.ts
+++ AngularjsQuickStart/AngularQS.Web/src/app/commonComponent/alert.component.ts
@@ -1,36 +1,45 @@
 import {Component,OnInit } from '@angular/core';
 import {AlertMessage} from '../models';
 import{AlertService} from '../services/alert.service';
+import {Subject} from 'rxjs'
+import {debounceTime} from 'rxjs/operators';
+
 
 @Component({
     selector:'app-alert',
-    templateUrl:'alert.component.html'
+    templateUrl:'alert.component.html'    
 })
 
 export class AlertComponent implements OnInit{
 
     constructor(private alertService:AlertService){
-
+           
     };
+    private _success = new Subject<string>();
+    successMessage: string;
+    isShow:boolean=false;
+
     message: AlertMessage;
     alerts: AlertMessage[]=[];
-    isHideAlertMessage:boolean;
+    
     ngOnInit() {
-        this.isHideAlertMessage=false;
-        var alertSubscribe=this.alertService.getMessage().subscribe(message => { 
+        var observableMessage = this.alertService.getMessage();
+        var alertSubscribe=observableMessage.subscribe(message => { 
             this.message = message as AlertMessage;             
             this.alerts.push(this.message);
+            this.isShow=true;
         });
-
-        setTimeout(() => {  
-            var xxx=this.alerts;          
-            this.isHideAlertMessage=true;
-          }, 3000);
+        let self=this;
+        observableMessage.pipe(
+            debounceTime(5000)
+        ).subscribe(() => {
+            self.alerts=[];
+            self.isShow=false;           
+        });
+        
      }
-
-     
 
      close(alert: AlertMessage) {
         this.alerts.splice(this.alerts.indexOf(alert), 1);
-      }
+    }
 }
