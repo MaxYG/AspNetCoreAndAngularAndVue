020e9270e0329bc886ad267276f7ad4e845197fa
H <harboryg@163.com>
2019-05-22 19:04:46 +0800

    create header and body framework

--- AngularjsQuickStart/VueQS/hello-world/src/App.vue
+++ AngularjsQuickStart/VueQS/hello-world/src/App.vue
@@ -1,40 +1,49 @@
 <template>
   <div id="app">
-    <h1>Hello App!</h1>
-   
-    <header>
-    <!-- router-link 定义点击后导航到哪个路径下 -->
-      <router-link to="/home">Home</router-link>
-      <router-link to="/about">About</router-link>
+     
+    <header>    
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
     </header>
-    <!-- 对应的组件内容渲染到router-view中 -->
+    
     <router-view></router-view>   
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-
 
 export default {
-  //name: 'app',
-  // components: {
-  //   HelloWorld
-  // }
-
-  // computed: {
-  //   username () {      
-  //     return this.$route.params.username
-  //   }
-  // },
-  // methods: {
-  //   goBack () {
-  //     window.history.length > 1
-  //       ? this.$router.go(-1)
-  //       : this.$router.push('/')
-  //   }
-  // }
-
+  name: 'app',
 }
 </script>
 
--- AngularjsQuickStart/VueQS/hello-world/src/routerManager.js
+++ AngularjsQuickStart/VueQS/hello-world/src/routerManager.js
@@ -1,11 +1,12 @@
 
 import VueRouter from 'vue-router'
-import home from './components/Home/Home.vue'
-import about from './components/Home/About.vue'
+//import home from './components/Home/Home.vue'
+//import about from './components/Home/About.vue'
+import main from './components/Home/Main'
 
 const routes=[
-    {path:"/home", component:home},
-    {path:"/about", component:about}
+    {path:"/", component:main},
+   
   ]
 
   var router=new VueRouter({
